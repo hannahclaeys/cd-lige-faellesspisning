586fc35599ddc023436b46cddca49e9ae6bdf4e4
hannahclaeys <hannahfog@gmail.com>
2025-04-28 22:36:48 +0200

    Update templates.js

--- templates.js
+++ templates.js
@@ -13,19 +13,31 @@ const templates = {
         <button onclick="loginUser()">Continue</button>
     `,
     newUser: `
-        <h2>Create your account</h2>
-        <input type="text" id="newUserName" placeholder="Enter your name"><br>
-        <label for="dietSelect">Dietary restrictions (choose one or more):</label><br>
-        <select id="dietSelect" multiple size="5" onchange="checkOtherOption()">
-            <option value="Vegetarian">Vegetarian</option>
-            <option value="Vegan">Vegan</option>
-            <option value="Gluten Free">Gluten Free</option>
-            <option value="Lactose Free">Lactose Free</option>
-            <option value="Other">Other</option>
-        </select><br>
-        <div id="otherDietDiv" class="hidden">
-            <input type="text" id="otherDiet" placeholder="Please specify">
-        </div><br>
-        <button onclick="registerNewUser()">Register</button>
-    `
+    <h2>Create your account</h2>
+    <input type="text" id="newUserName" placeholder="Enter your name"><br>
+
+    <label for="dietSelect">Dietary restrictions (choose one or more):</label><br>
+    <select id="dietSelect" multiple size="5" onchange="checkOtherOption()">
+        <option value="Vegetarian">Vegetarian</option>
+        <option value="Vegan">Vegan</option>
+        <option value="Gluten Free">Gluten Free</option>
+        <option value="Lactose Free">Lactose Free</option>
+        <option value="Other">Other</option>
+    </select><br>
+
+    <div id="otherDietDiv" class="hidden">
+        <input type="text" id="otherDiet" placeholder="Please specify">
+    </div><br>
+
+    <h3>Choose your avatar</h3>
+    <div id="avatarSelection">
+        <img src="avatars/avatar1.png" class="avatar" onclick="selectAvatar('avatar1.png')">
+        <img src="avatars/avatar2.png" class="avatar" onclick="selectAvatar('avatar2.png')">
+        <img src="avatars/avatar3.png" class="avatar" onclick="selectAvatar('avatar3.png')">
+        <img src="avatars/avatar4.png" class="avatar" onclick="selectAvatar('avatar4.png')">
+    </div>
+    <br>
+
+    <button onclick="registerNewUser()">Register</button>
+`
 };
