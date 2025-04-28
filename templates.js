7e47e44fa597970f466c61a422ea857ab2a86b34
hannahclaeys <hannahfog@gmail.com>
2025-04-28 23:13:06 +0200

    Update templates.js

--- templates.js
+++ templates.js
@@ -2,42 +2,50 @@
 
 const templates = {
     welcome: `
-        <h1>Welcome to CD-lige Kitchen Dinners!</h1>
-        <p>Who are you?</p>
-        <button onclick="showExistingUser()">Choose existing user</button>
-        <button onclick="showNewUserForm()">I'm new! Create account</button>
+        <div class="fade-in">
+            <h1>Welcome to CD-lige Kitchen Dinners!</h1>
+            <p>Who are you?</p>
+            <button onclick="showExistingUser()">Choose existing user</button>
+            <button onclick="showNewUserForm()">I'm new! Create account</button>
+        </div>
     `,
     existingUser: `
-        <h2>Select your name</h2>
-        <select id="userList"></select><br>
-        <button onclick="loginUser()">Continue</button>
+        <div class="fade-in">
+            <h2>Select your name</h2>
+            <select id="userList"></select><br>
+            <button onclick="loginUser()">Continue</button>
+        </div>
     `,
     newUser: `
-    <h2>Create your account</h2>
-    <input type="text" id="newUserName" placeholder="Enter your name"><br>
+        <div class="fade-in">
+            <h2>Create your account</h2>
+            <input type="text" id="newUserName" placeholder="Enter your name"><br>
 
-    <label for="dietSelect">Dietary restrictions (choose one or more):</label><br>
-    <select id="dietSelect" multiple size="5" onchange="checkOtherOption()">
-        <option value="Vegetarian">Vegetarian</option>
-        <option value="Vegan">Vegan</option>
-        <option value="Gluten Free">Gluten Free</option>
-        <option value="Lactose Free">Lactose Free</option>
-        <option value="Other">Other</option>
-    </select><br>
+            <label for="dietSelect">Dietary restrictions (choose one or more):</label><br>
+            <select id="dietSelect" multiple size="5" onchange="checkOtherOption()">
+                <option value="Vegetarian">Vegetarian</option>
+                <option value="Vegan">Vegan</option>
+                <option value="Gluten Free">Gluten Free</option>
+                <option value="Lactose Free">Lactose Free</option>
+                <option value="Other">Other</option>
+            </select><br>
 
-    <div id="otherDietDiv" class="hidden">
-        <input type="text" id="otherDiet" placeholder="Please specify">
-    </div><br>
+            <div id="otherDietDiv" class="hidden">
+                <input type="text" id="otherDiet" placeholder="Please specify">
+            </div><br>
 
-    <h3>Choose your avatar</h3>
-    <div id="avatarSelection">
-        <img src="avatars/avatar1.png" class="avatar" onclick="selectAvatar('avatar1.png')">
-        <img src="avatars/avatar2.png" class="avatar" onclick="selectAvatar('avatar2.png')">
-        <img src="avatars/avatar3.png" class="avatar" onclick="selectAvatar('avatar3.png')">
-        <img src="avatars/avatar4.png" class="avatar" onclick="selectAvatar('avatar4.png')">
-    </div>
-    <br>
+            <h3>Choose your avatar</h3>
+            <div id="avatarSelection">
+                <img src="avatars/avatar1.png" class="avatar" onclick="selectAvatar('avatar1.png')">
+                <img src="avatars/avatar2.png" class="avatar" onclick="selectAvatar('avatar2.png')">
+                <img src="avatars/avatar3.png" class="avatar" onclick="selectAvatar('avatar3.png')">
+                <img src="avatars/avatar4.png" class="avatar" onclick="selectAvatar('avatar4.png')">
+            </div>
+            <br>
 
-    <button onclick="registerNewUser()">Register</button>
-`
+            <button onclick="registerNewUser()">Register</button>
+            <br><br>
+            <button onclick="loadTemplate('welcome')">Back</button>
+        </div>
+    `
 };
